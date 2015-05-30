1446725a334baad58021064a624d36dda64040e6
Dima Malofeev <dim2k2006@yandex.ru>
2015-05-30 21:45:02 +0300

    ДЗ «Сайт портфолио» ­ часть 1

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,51 @@ var gulp = require('gulp'),
 	gulpif = require('gulp-if'),
 	uglify = require('gulp-uglify'),
 	minifyCss = require('gulp-minify-css'),
-	clean = require('gulp-clean');
+	clean = require('gulp-clean'),
+	connect = require('gulp-connect'),
+	opn = require('opn'),
+	notify = require("gulp-notify");
+
+
+
+// Web server + livereload
+gulp.task('connect', function() {
+	connect.server({
+		root: 'app',
+		livereload: true,
+		port: 8888
+	});
+	opn('http://localhost:8888');
+});
+
+
+
+// Work with html
+gulp.task('html', function () {
+	gulp.src('./app/*.html')
+	.pipe(connect.reload());
+});
+
+
+
+// Work with css
+gulp.task('css', function () {
+	gulp.src('./app/css/*.css')
+	.pipe(connect.reload());
+});
+
+
+
+// Work with js
+gulp.task('js', function () {
+	gulp.src('./app/js/*.js')
+	.pipe(connect.reload());
+});
+
 
 
 // Build
-gulp.task('build', ['clean'], function () {
+gulp.task('build', ['move'], function () {
     var assets = useref.assets();
     
     return gulp.src('app/*.html')
@@ -30,6 +70,17 @@ gulp.task('clean', function () {
 
 
 
+// Move images and fonts to dist
+gulp.task('move', ['clean'], function() {
+	gulp.src('./app/images/*')
+	.pipe(gulp.dest('dist/images'));
+
+	gulp.src('./app/fonts/*')
+	.pipe(gulp.dest('dist/fonts'));
+});
+
+
+
 // Bower wiredep
 gulp.task('bower', function () {
 	gulp.src('./app/*.html')
@@ -41,8 +92,15 @@ gulp.task('bower', function () {
 
 
 
-// Bower watch
+// Watch
 gulp.task('watch', function () {
+	gulp.watch(['./app/*.html'], ['html']);
+	gulp.watch(['./app/css/*.css'], ['css']);
+	gulp.watch(['./app/js/*.js'], ['js']);
 	gulp.watch('bower.json', ['bower']);
-})
+});
 
+
+
+// Default task
+gulp.task('default', ['connect', 'watch']);
