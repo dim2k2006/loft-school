5d109f92eddbc46fc928744d07b831ab5ee5aff6
Dima Malofeev <dim2k2006@yandex.ru>
2015-06-06 21:57:31 +0300

    js placeholder

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-	console.log('main');
+	var inputs = $('.form__input');
+
+	inputs.each(function() {
+    	$(this)
+		.data('default', $(this).val())
+		.addClass('inactive')
+		.focus(function() {
+    		$(this).removeClass('inactive');
+	        if($(this).val() === $(this).data('default') || $(this).val() === '') {
+	        	$(this).val('');
+	        }
+		})
+		.blur(function() {
+		if($(this).val() === '') {
+		  	$(this).addClass('inactive').val($(this).data('default'));
+		}
+		});
+	});
 })
