a22a850ce7a63de401e1d0bf559d761d1629d4b4
Dima Malofeev <dim2k2006@yandex.ru>
2015-07-10 23:41:47 +0300

    form validation

--- app/js/validation.js
+++ app/js/validation.js
@@ -4,6 +4,7 @@
 	 */
 	$('.tooltip').each(function(index, el) {
 		var position = $(this).attr('data-position') || 'left';
+		
 		$(this).tooltipster({
 			autoClose: 'false',
 			trigger: 'custom',
@@ -27,13 +28,80 @@
 			data = $(this).val();
 			mask = $(this).attr('data-mask');
 			id = $(this).attr('id');
-			if (data === mask || data === '' || data === ' ') {
-				$(this).addClass('form__error');
-				$('#'+id).tooltipster('show');
+
+			if (id === 'project-attach') {
+				if (data === mask || data === '' || data === ' ') {
+					$(this).parent().addClass('form__error');
+					$('#'+id).parent().tooltipster('show');
+				} else {
+					$(this).parent().removeClass('form__error');
+					$('#'+id).parent().tooltipster('hide');
+				}
 			} else {
-				$(this).removeClass('form__error');
-				$('#'+id).tooltipster('hide');
+				if (data === mask || data === '' || data === ' ') {
+					$(this).addClass('form__error');
+					$('#'+id).tooltipster('show');
+				} else {
+					$(this).removeClass('form__error');
+					$('#'+id).tooltipster('hide');
+				}
 			}
 		});		
+	});
+
+
+
+	/**
+	 * Input Keyup
+	 */
+	$('.form__input').keyup(function(event) {
+		var id = $(this).attr('id');
+
+		if ($(this).hasClass('form__error')) {
+			$(this).removeClass('form__error');
+			$('#'+id).tooltipster('hide');
+		}
+	});
+
+
+
+	/**
+	 * Input Type File Change
+	 */
+	$('.modal__upload-input').change(function(event) {
+		var id = $(this).attr('id');
+
+		$(this).parent().removeClass('form__error');
+		$('#'+id).parent().tooltipster('hide');
+	});
+
+
+
+	/**
+	 * Form Reset
+	 */
+	$('.form-reset').click(function(event) {
+		var container = $(this).parent().parent().parent(),
+			id = '';
+
+		
+
+		$('.form__input', container).each(function(index, el) {
+			id = $(this).attr('id');
+			
+			$(this).removeClass('form__error');
+			$('#'+id).tooltipster('hide');
+		});	
+	});
+
+
+
+	/**
+	 * Hide tooltips and reset form state after modal close
+	 */
+	$('#add-project').on('hidden.bs.modal', function () {
+    	$('.tooltip').tooltipster('hide');
+    	$('.form__input', this).removeClass('form__error');
+    	$('.modal__upload-box', this).removeClass('form__error');
 	});
 });
